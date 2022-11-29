45b97d02e725d23b6c9b5623f638a0687a95c7b0
Tejas-2005 <106252704+Tejas-2005@users.noreply.github.com>
2022-11-29 16:26:52 +0530

    Update Railway Reservation System.py

--- Railway Reservation System.py
+++ Railway Reservation System.py
@@ -1,6 +1,7 @@
 import mysql.connector as sql 
 import time as t 
 import random as R
+import pandas as pd
 
 mysql = sql.connect(host = "localhost",user="root",password = "",database="Railway")
 
@@ -31,7 +32,11 @@ def work():
                     a = "SELECT * FROM train"
                     mycursor.execute(a)
                     for i in mycursor:
-                        print(i)
+                        print("")
+                        a = ["Train Name","Train No.","Journey From","Destination"]
+                        run = pd.DataFrame([i],columns=a,index=[""])
+                        print(run)
+                        print("")
                     print("Succesfully added !!!")
                     mysql.commit()
                     print("Want to add More ")
@@ -44,8 +49,11 @@ def work():
                     b = "SELECT * FROM train"
                     mycursor.execute(b)
                     for i in mycursor:
-                        print("Train Name","Train No.","Journey From","Destination")
-                        print(i)
+                        print("")
+                        a = ["Train Name","Train No.","Journey From","Destination"]
+                        run = pd.DataFrame([i],columns=a,index=[""])
+                        print(run)
+                        print("")
                     mysql.commit()
                     print("Which Train Has to Be Deleted From Above List")
                     train_no = input("Enter Train Number : ")
@@ -56,8 +64,11 @@ def work():
                     mycursor.execute(v)
                     t.sleep(1)
                     for i in mycursor:
-                        print("Train Name","Train No.","Journey From","Destination")
-                        print(i)
+                        print("")
+                        a = ["Train Name","Train No.","Journey From","Destination"]
+                        run = pd.DataFrame([i],columns=a,index=[""])
+                        print(run)
+                        print("")
                     mysql.commit()
                     print("Want to add More ")
                     YES = input("YES OR NO .. (Type Y/N) \n")
@@ -103,23 +114,23 @@ def reservation():
                 l1.append(cls)           
                 print("Total amount to be paid:",amount)
                 l1.append(amount)
-                pnr = R.randrange(10000,500000)
+                pnr = R.randrange(99999,999999)
                 print("PNR Number:",pnr)
                 print("status: confirmed")
                 sts='confirmed'
                 l1.append(sts)
                 l1.append(pnr)
                 train1=(l1)
-                cr = "create table if not exists passengers(pname varchar(183),age varchar(183),trainno varchar(183),noofpas varchar(183),cls varchar(183),amt varchar(183),status varchar(183),pnrno varchar(183))"
+                cr = "create table if not exists passengers(pname varchar(183),age varchar(183),trainno varchar(183),noofpas varchar(183),cls varchar(183),amt varchar(183),status varchar(183),pnrno varchar(183),primary key(pnrno))"
                 sql="insert into passengers(pname,age,trainno,noofpas,cls,amt,status,pnrno)values(%s,%s,%s,%s,%s,%s,%s,%s)"
                 mycursor.execute(cr)
                 t.sleep(1)
                 mycursor.execute(sql,train1)
                 join = "select * from train,passengers where train.train_no = passengers.trainno"
                 mycursor.execute(join)
-                print("Train Name,Train No.,Journey From,Destination,Passenger name ,Age,Train No.,No of Passenger,Class,Amount,Status,PNR No")
                 for y in mycursor:
-                    print(y)
+                    ew = [y]
+                    a = pd.DataFrame(ew)
                     print("Your PNR no. :",pnr)
                     print("TICKET")
                 mysql.commit()
@@ -155,13 +166,13 @@ def displayPNR():
                 pnr=input("enter PNR NUMBER")
                 pn=(pnr,) 
                 sql= "select * from passengers where pnrno=%s "
-                mycursor.execute(sql,pn)
-                res= mycursor.fetchall() 
+                mycursor.execute(sql,pn) 
                 #mydb.commit()
                 print("PNR STATUS are as follows : ")
-                a = ("(Passenger name,age,train no, no of passenger,class,amount,status, PNR)")
-                for x in res:
-                                print(x)   
+                for x in mycursor:
+                                x1 = [x]
+                                y = pd.DataFrame(x1,columns=["Name","Age","Train no.","No of passenger","Class","Amount","Status","PNR"],index=[""])
+                                print(y)   
                 #print("Deletion completed")
                 print("Go back to menu OR Skip ")
                 opt = input("Y for menu or N to ")
@@ -192,11 +203,7 @@ def railresmenu():
     t.sleep(1.5)
 
 
-
-def users():
-            print("Please Sign In \n If doesn't have account go to different portal:")
-            OPTION = input("Want to \n1. Sign In \n2. Sign UP : ")
-            if OPTION == "1":
+def sign_in():
                 Name = input("Enter Your Name : ")
                 Age = input("Enter Your Age : ")
                 Sex = input("Enter Your Sex M/F : ")
@@ -205,17 +212,23 @@ def users():
                     print("")
                 else:
                     print("Plzz input correct no.")
-                det = [Name,Age,Sex,Mob]
+                det = [Name,Age,Mob]
+                if Sex == "M":
+                    det.append("Male")
+                else:
+                    det.append("Female")
                 print("Please set a password of mininmum 6 digits : ")
                 pswad = input("")
                 det.append(pswad)
                 deta = (det)
-                w = "create table if not exists user(Name varchar(183),Age varchar(183),Sex varchar(5),Mob varchar(183),Password varchar(183))"
-                w1 = "insert into user(Name,Age,Sex,Mob,Password) values(%s,%s,%s,%s,%s)"
+                w = "create table if not exists user(Name varchar(183),Age varchar(183),Sex varchar(6),Mob varchar(183),Password varchar(183),unique(Mob))"
+                w1 = "insert into user(Name,Age,Mob,Sex,Password) values(%s,%s,%s,%s,%s)"
                 mycursor.execute(w)
                 mycursor.execute(w1,deta)
                 mysql.commit()
-            elif OPTION == "2":
+                railresmenu()
+
+def sign_up():
                 x1 = "select Password from user"
                 mycursor.execute(x1)
                 a1 = mycursor.fetchall()
@@ -223,14 +236,28 @@ def users():
                     print()
                 Q  = input("Enter your password : ")
                 Q2 = (Q,)
-                if Q in i:
-                    x2 = "select * from user where Password = %s"
-                    mycursor.execute(x2,Q2)
-                    for i in mycursor:
-                        print("","\n","Name",i[0] ,"\n","Age",i[1],"\n","Sex",i[2],"\n","Mob",i[3],"\n")
-            railresmenu()
+                x2 = "select * from user where Password = %s"
+                mycursor.execute(x2,Q2)
+                for i in mycursor:
+                        print("Name :",i[0])
+                        print("Age :",i[1])
+                        print("Sex :",i[2])
+                        print("Mobile no :",i[3])
+                        print("")
+                        railresmenu()
 
-print("Please login first !")
+def users():
+            print("Please Sign In \n If doesn't have account go to different portal:")
+            OPTION = input("Want to \n1. Sign In \n2. Sign UP : ")
+            if OPTION == "1":
+                sign_in()
+            elif OPTION == "2":
+                sign_up()
+
+
+
+
+print("\t Please login first ! \t")
 log = input("""Want to Sign as :
                 1.Admin
                 2.User
@@ -246,7 +273,5 @@ if log == "Admin":
         work()
 elif log == "user":
     users()
-
-
 
 
